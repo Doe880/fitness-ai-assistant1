e25ae4be609e8097cfbe1c150883f5a74f1bcd53
ivanov.a <lobanovsky23@mail.ru>
2026-05-13 15:50:54 +0300

    16

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, EmailStr, Field
 
 class RegisterRequest(BaseModel):
     email: EmailStr
-    password: str = Field(..., min_length=6)
+    password: str = Field(..., min_length=6, max_length=72)
 
 
 class LoginRequest(BaseModel):
